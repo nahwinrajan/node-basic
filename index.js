c3351e8c2999e5df267a28e73dc032d9601a678d
nahwinrajan <nahwin.rajan@gmail.com>
2016-09-02 13:10:21 +0700

    completed handle reques and styling

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const mimeTypes = {
+  'html':'text/html',
+  'jpeg': 'image/jepg',
+  'jpg': 'image/jpg',
+  'png': 'image/png',
+  'js': 'text/javascript',
+  'css': 'text/css'
+}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+http.createServer(function(req, res){
+  let uri = url.parse(req.url).pathname;
+  let fileName = path.join(process.cwd(), unescape(uri));
+  console.log('Loading: ' + uri);
+  console.log('file name: ' + fileName);
+  let stats;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  try {
+    stats = fs.lstatSync(fileName);
+  } catch(e) {
+    res.writeHead(404, { 'Content-type': 'text/plain' });
+    res.write('Content not found.... \n');
+    res.end();
+    return;
+  }
+
+  //if requested file exist
+  if (stats.isFile()){
+    //get the extension of the file
+    let mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+    res.writeHead(200, { 'Content-type': mimeType });
+
+    let filesStream = fs.createReadStream(fileName);
+    filesStream.pipe(res);
+  } else if (stats.isDirectory()) {
+    res.writeHead(302, { 'Location': 'index.html' });
+    res.end();
+  } else {
+    res.writeHead(500, { 'Content-type': 'text/plain' });
+    res.write('Internal server error.... :( ');
+    res.end();
+  }
+
+}).listen(1337);
